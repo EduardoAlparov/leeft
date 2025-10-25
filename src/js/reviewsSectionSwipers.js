769c70f9a0a95daf70fae3fa9e100443df6d029f
EduardoAlparov <workman133@yandex.ru>
2025-10-25 04:56:08 +0300

    dev: new swiper review 24.10.25

--- src/js/reviewsSectionSwipers.js
+++ src/js/reviewsSectionSwipers.js
@@ -2,10 +2,13 @@ import Swiper from 'swiper';
 import { Navigation, Freemode, Pagination, Controller, EffectFade, Autoplay } from 'swiper';
 
 import { IS_TABLET } from './utils';
+import { IS_MOBILE } from './utils';
+import { IS_DESKTOP } from './utils';
 
 
 export default () => {
     const sections = document.querySelectorAll('.js-reviews-section');
+    const els = document.querySelectorAll('.js-reviews-carousel');
 
     sections.forEach( (section) => {
         const textSwiperBox = section.querySelector('.reviews-section__text-swiper.swiper');
@@ -63,6 +66,65 @@ export default () => {
 
             pagination: {
                 el: section.querySelector('.reviews-section__pagination'),
+                type: 'fraction'
+            }
+        });
+    })
+
+    els.forEach((el) => {
+        const textSwiperBox = el.querySelector('.reviews__text-swiper.swiper');
+        const imagesSwiperBox = el.querySelector('.reviews__gallery-swiper.swiper');
+        const thumbsSwiperBox = el.querySelector('.reviews__thumbs-swiper.swiper');
+        let thumbsSwiper;
+
+        if(!textSwiperBox | !imagesSwiperBox) return;
+
+        if(IS_DESKTOP) {
+            imagesSwiperBox.setAttribute('dir', 'rtl')
+        }
+
+        const imageSwiper = new Swiper(imagesSwiperBox, {
+            modules: [Navigation, Controller, EffectFade],
+
+            speed: 700,
+            slidesPerView:  IS_DESKTOP ? 3.1 : 1,
+            spaceBetween: 8,
+            loop: IS_DESKTOP,
+            loopAdditionalSlides: 20,
+            allowTouchMove: false,
+
+
+            // freeMode: {
+            //     enabled: IS_DESKTOP,
+            //     sticky: true,
+            //   },
+        });
+
+        const textSwiper = new Swiper(textSwiperBox, {
+            modules: [Navigation, Pagination, Controller, EffectFade],
+            speed: 700,
+            slidesPerView: 1,
+            spaceBetween: 8,
+            loop: IS_DESKTOP,
+            loopAdditionalSlides: 10,
+            allowTouchMove: IS_MOBILE ? true : false,
+
+            effect: IS_MOBILE ? 'slide' : 'fade',
+            fadeEffect: {
+                crossFade: true,
+            },
+
+            controller: {
+                control: [imageSwiper]
+            },
+
+            navigation: {
+                nextEl: el.querySelector('.reviews-next-btn'),
+                prevEl: el.querySelector('.reviews-prev-btn')
+            },
+
+            pagination: {
+                el: el.querySelector('.reviews__pagination'),
                 type: 'fraction'
             }
         });
--- src/js/utils.js
+++ src/js/utils.js
@@ -7,7 +7,7 @@ export function convertRemToPixels(rem) {
     return rem * parseFloat(getComputedStyle(document.documentElement).fontSize);
 }
 
-export const IS_MOBILE = window.matchMedia('(max-width: 640px)').matches;
+export const IS_MOBILE = window.matchMedia('(max-width: 743px)').matches;
 export const IS_TABLET = window.matchMedia('(max-width: 1200px)').matches;
 export const IS_DESKTOP = window.matchMedia('(min-width: 1200px)').matches;
 export const CAN_HOVER = window.matchMedia("(hover: hover)").matches;
